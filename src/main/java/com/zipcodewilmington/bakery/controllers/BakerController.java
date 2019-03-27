c77b7cc6fae74839ccb786edf51948af28e6bac9
David Tamulavage <davidtamulavage@zipcoders-MacBook-Pro-2.local>
2019-03-27 08:14:00 -0400

    first try with old testcases. Megering to get tnew test cases

--- src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
@@ -4,7 +4,13 @@ import com.zipcodewilmington.bakery.models.Baker;
 import com.zipcodewilmington.bakery.services.BakerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class BakerController {
     private BakerService service;
 
@@ -12,23 +18,28 @@ public class BakerController {
         this.service = service;
     }
 
+
     public ResponseEntity<Iterable<Baker>> index() {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
 
-    public ResponseEntity<Baker> show(Long id) {
+    @RequestMapping(value = "/show.html", method = RequestMethod.GET)
+    public ResponseEntity<Baker> show(@PathVariable Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
 
-    public ResponseEntity<Baker> create(Baker baker) {
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public ResponseEntity<Baker> create(@RequestParam Baker baker) {
         return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
     }
 
-    public ResponseEntity<Baker> update(Long id, Baker baker) {
+    @RequestMapping(value = "/update", method = RequestMethod.PUT)
+    public ResponseEntity<Baker> update(@RequestParam Long id, @RequestParam Baker baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
 
-    public ResponseEntity<Boolean> destroy(Long id) {
+    @RequestMapping(value = "/destroy", method = RequestMethod.DELETE)
+    public ResponseEntity<Boolean> destroy(@RequestParam Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
 }
